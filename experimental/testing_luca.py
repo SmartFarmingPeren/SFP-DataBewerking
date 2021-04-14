5287967cebbd45ccc2af653185f9429d5fc63043
Luca van Elsas <lucavanelsas@gmail.com>
2021-04-14 20:04:58 +0200

    removed test and repurposed its code

--- experimental/testing_luca.py
+++ experimental/testing_luca.py
@@ -1,5 +1,26 @@
+from pyvis.network import Network
+import networkx as nx
+
+from openalea.plantgl.all import *
+
+from Skeletonization.main import skeleton
+from utilities.configuration_file import *
+
 def main():
-    print("")
+    input_point_cloud = "gen_2_23_03_expanded.ply"
+    scene = Scene(INPUT_POINT_CLOUDS_DIR + input_point_cloud)
+    points = scene[0].geometry.pointList
+    
+    mtg = skeleton(points)
+    nx_graph = nx.cycle_graph(10)
+    nx_graph.nodes("MTG(mtg)")
+
+
+    nt = Network('500px', '500px')
+    # populates the nodes and edges data structures
+    nt.from_nx(nx_graph)
+    nt.show('nx.html')
+
 
 if __name__ == '__main__':
     main()
