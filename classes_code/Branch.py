849e57efa6a02d3a34ce8e320f2862301df48ca3
Brandist <brandondisseldorp@hotmail.com>
2021-05-27 14:58:14 +0200

    Revert "Merge branch 'Leider-technologie' into main"
    This reverts commit 2b4584f50585774c62aedfb2799ab2440f0a7954, reversing
    changes made to 6512b7db55b6f8686c94808015f7920cac65c6d4.

--- classes_code/Branch.py
+++ classes_code/Branch.py
@@ -1,3 +1,5 @@
+from openalea.plantgl.math import Vector3
+
 from classes_code.Point import Point
 
 
@@ -16,6 +18,7 @@ class Branch:
         self.depth = depth
         self.age = age
         self.is_leader = is_leader
+        self.is_pruned = False # This is a shit name but i could not think of something better - Luca (27-05-2021)
 
     def next(self, point: Point):
         """
@@ -32,11 +35,10 @@ class Branch:
         """
         return "%s: \n" \
                "Age: %d \n" \
-               "Is Leader: %s \n" \
                "Parent: %s \n" \
                "Children" "%s \n" \
                "Points: \n \t %s" % (
-                   self.id, self.age, self.is_leader, self.parent, self.children, [section.__str__() for section in self.points])
+                   self.id, self.age, self.parent, self.children, [section.__str__() for section in self.points])
         pass
 
     # start_point is always a +N point of the branch
@@ -57,7 +59,6 @@ class Branch:
                 save each point with 1 child
                 save n+ points
         """
-        #for point in mtg.Sons(start_point):
 
         # Create branch points, first one being the start_point
         self.points = [Point.from_mtg(mtg, start_point)]
@@ -99,6 +100,7 @@ class Branch:
                         continue
                 if old_point == current_point:
                     break
+
 
 def get_next(node):
     """
